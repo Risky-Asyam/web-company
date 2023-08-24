315010373517ec68a52132dfcf8d1d0e64c3540f
Risky-Asyam <riskyasyam@gmail.com>
2023-08-24 19:55:10 +0700

    Update halaman.php

--- admin/halaman.php
+++ admin/halaman.php
@@ -3,9 +3,24 @@
 <head>  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/logo.png" type="image/x-icon">
     <title>Admin Company Profile</title>
 </head>
 <body>
-
+    <div id = "header">
+        <div class = "navbar">
+            <nav>
+                <img class = "logo" src="../assets/logo.png" width = "50" height = "50">
+                <p class = "nama-logo">SMC_IT</p>
+                <ul>
+                    <li><a href = "#">Home</a></li>
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </body>
 </html>
