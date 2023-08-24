51f057c52a2a41d828a9bd08f216cf77525c466d
Risky-Asyam <riskyasyam@gmail.com>
2023-08-24 23:04:18 +0700

    Update halaman.php

--- admin/halaman.php
+++ admin/halaman.php
@@ -42,20 +42,20 @@
   </header>
   <main>
     <?php
-    $katakunci = (isset($_GET['katakunci']))?$_GET['katakunci']:"";
+    $katakunci = (isset($_GET['katakunci'])) ? $_GET['katakunci'] : "";
     ?>
     <h1>Halaman Admin</h1>
     <p>
       <a href="halaman_input.php">
-        <input type="button" class="btn btn-primary" value="Buat Halaman Baru"/>
+        <input type="button" class="btn btn-primary" value="Buat Halaman Baru" />
       </a>
     </p>
     <form class="row g-3" method="get">
       <div class="col-auto">
-        <input type="text" class="form-control" placeholder="Masukkan Kata Kunci" name="katakunci" value="<?php echo $katakunci?>"/>
+        <input type="text" class="form-control" placeholder="Masukkan Kata Kunci" name="katakunci" value="<?php echo $katakunci ?>" />
       </div>
       <div class="col-auto">
-        <input type="submit" name="cari" value="Cari Tulisan" class="btn btn-secondary"/>
+        <input type="submit" name="cari" value="Cari Tulisan" class="btn btn-secondary" />
       </div>
     </form>
     <table class="table table-striped">
@@ -64,9 +64,20 @@
           <th class="col-1">#</th>
           <th>Judul</th>
           <th>Kutipan</th>
-          <th class="col-1">Aksi</th>
+          <th class="col-2">Aksi</th>
         </tr>
       </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, nobis.</td>
+          <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+          <td>
+            <span class="badge text-bg-warning">Edit</span>
+            <span class="badge text-bg-danger">Delete</span>
+          </td>
+        </tr>
+      </tbody>
     </table>
   </main>
   <footer class="bg-light">
