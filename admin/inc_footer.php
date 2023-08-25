e378a9352075675688493d708905e40d8a383165
Risky-Asyam <riskyasyam@gmail.com>
2023-08-25 07:19:02 +0700

    Update inc_footer.php

--- admin/inc_footer.php
+++ admin/inc_footer.php
@@ -7,31 +7,31 @@
 <script>
     $(document).ready(function() {
         $('#summernote').summernote({
-            callbacks:{
-                onImageUpload: function(files){
-                    for(let i = 0; i < files.lenght; i++) {
+            callbacks: {
+                onImageUpload: function(files) {
+                    for (let i = 0; i < files.length; i++) {
                         $.upload(files[i]);
                     }
                 }
             },
-            height:200
+            height: 200
         });
 
         $.upload = function(file) {
             let out = new FormData();
             out.append('file', file, file.name);
 
-            $ajax({
+            $.ajax({
                 method: 'POST',
                 url: 'upload_gambar.php',
                 contentType: false,
                 cache: false,
                 processData: false,
                 data: out,
-                success: function (img) {
+                success: function(img) {
                     $('#summernote').summernote('insertImage', img);
                 },
-                error: function (jqXHR, textStatus, errorThrown) {
+                error: function(jqXHR, textStatus, errorThrown) {
                     console.error(textStatus + " " + errorThrown);
                 }
             });
